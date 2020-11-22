7ba542c90583e26749634aa3008306522fbef323
Felix <felixpb@yahoo.com.br>
2020-11-22 15:17:10 -0300

    Tarefa 5
    • Tarefa 5 - Projeto lógico e físico de um banco de dados para um Hotel (Incompleto)

--- Exercícios/Tarefa 5 - Projeto lógico e físico de um banco de dados para um Hotel/Ex2 - Script.sql
+++ Exercícios/Tarefa 5 - Projeto lógico e físico de um banco de dados para um Hotel/Ex2 - Script.sql
@@ -1,7 +1,9 @@
+/* Exercício a */
 create database HotelDB;
 
 use HotelDB;
 
+/* Exercício b */
 create table Hospede (
     cpf char(11) primary key,
     rg char(10) unique not null,
@@ -85,3 +87,31 @@ create table ConsumoProduto (
     foreign key(codConsumo) references Consumo(codConsumo),
     foreign key(codProduto) references ProdutoFrigobar(codProduto)
 );
+
+/* Exercício c */
+create view v_hospedagem as select numeroQuarto, dataEntrada, nome as nomeHospede, cpfHospede, valorDiaria, codSolicitacao, s.dataHora as dataHoraSolicitacao, s.nomeSolicitante as solicitanteSolicitacao, codConsumo, c.dataHora as dataHoraConsumo, c.nomeSolicitante as solicitanteConsumo from Hospedagem hgem left join Hospede on cpfHospede = cpf left join Solicitacao s on hgem.codHospedagem = s.codHospedagem left join Consumo c on hgem.codHospedagem = c.codHospedagem;
+
+create view v_solicitacoes as select cpfHospede, dataHora, nomeSolicitante, descricao, valor, quantidade, (valor*quantidade) as total from Solicitacao s left join Hospedagem h on s.codHospedagem = h.codHospedagem left join SolicitacaoServico ss on s.codSolicitacao = ss.codSolicitacao left join Servico serv on ss.codServico = serv.codServico;
+
+create view v_consumos as select cpfHospede, dataHora, nomeSolicitante, descricao, valor, quantidade, (valor*quantidade) as total from Consumo c left join Hospedagem h on c.codHospedagem = h.codHospedagem left join ConsumoProduto cp on c.codConsumo = cp.codConsumo left join ProdutoFrigobar pf on cp.codProduto = pf.codProduto;
+
+/* Exercício e */
+insert into Hospede values
+    ('31415926536', '562951413', 'Pedrinho Inquieto', '1996-10-05', 'Rua das pamonhas, 422, São Paulo, SP'),
+    ('56977820014', '446687452', 'Maria Rodrigues', '1984-04-25', 'Rua caleigos, 22, São Paulo, SP'),
+    ('44689952143', '555478299', 'Laura Morton', '2000-11-06', 'Av. burgmuller, 101, Santo André, SP')
+;
+
+/* Exercício f */
+
+
+/* Exercício g */
+
+
+/* Exercício h */
+
+
+/* Exercício i */
+
+
+/* Exercício j */
